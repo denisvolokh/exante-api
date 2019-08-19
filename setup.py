5561bc0e36c64a19ab6addd7f590742fef48bfb2
Denis Volokh <denis.volokh@gmail.com>
2019-08-19 17:15:13 +0800

    fixed setup py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requires = [
 
 setuptools.setup(
    name='exante',
-   version=exanteapi.__version__,
+   version=exante.__version__,
    description='Exante REST API python library',
    author='Denis Volokh',
    author_email='denis.volokh@gmail.com',
