a22b558df105eb5e4d111c825fa742774e2f039b
Denis Volokh <denis.volokh@gmail.com>
2019-09-14 17:32:03 +0800

    fixed setup, version 0.2.1

--- setup.py
+++ setup.py
@@ -2,22 +2,18 @@ import setuptools
 
 import exante
 
-packages = setuptools.find_packages(exclude=[
-    'exante.tests',
-    'exante.tests.integration',
-    'exante.tests.unit'
-])
-requires = [
-    'requests'
-]
+packages = setuptools.find_packages(
+    exclude=["exante.tests", "exante.tests.integration", "exante.tests.unit"]
+)
+requires = ["requests", "pyjwt"]
 
 
 setuptools.setup(
-   name='exante',
-   version=exante.__version__,
-   description='Exante REST API python library',
-   author='Denis Volokh',
-   author_email='denis.volokh@gmail.com',
-   packages=packages,
-   install_requires=requires
+    name="exante",
+    version=exante.__version__,
+    description="Exante REST API python library",
+    author="Denis Volokh",
+    author_email="denis.volokh@gmail.com",
+    packages=packages,
+    install_requires=requires,
 )
